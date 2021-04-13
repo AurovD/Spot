ee96b987eeae52bbe2e05f2b1f3a38d77103b2cd
AurovD <dmitry8891@mail.ru>
2021-04-13 12:03:44 +0300

    Are ready for git clone?

--- server/api/models/bd.js
+++ server/api/models/bd.js
@@ -1,8 +1,16 @@
 const Pool = require('pg').Pool
+// const pool = new Pool({
+//     user: 'babaduk',
+//     host: 'localhost',
+//     database: 'spot',
+//     password: '123',
+//     port: 5432,
+// });
+
 const pool = new Pool({
-    user: 'babaduk',
+    user: 'postgres',
     host: 'localhost',
-    database: 'spot',
+    database: 'portfolio',
     password: '123',
     port: 5432,
 });
