c95f70c4076f9d1c4dc79f676409f5e22bae1f88
AurovD <dmitry8891@mail.ru>
2021-05-20 00:42:23 +0300

    test connection

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -4,9 +4,21 @@ const usr = require("../controllers/users");
 const evt = require("../controllers/events");
 const parser = require("body-parser").json();
 const cors = require("cors");
+
+let whitelist = ['http://spot.aurovd.ru']
+let corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
 router.post("/signup", parser, usr.signup);
 router.post("/login", parser, usr.login);
-router.get("/test", cors(),parser, usr.test);
+router.get("/test", cors(corsOptions),parser, usr.test);
 router.post("/createEvent", parser, evt.createEvent);
 router.get("/fetchMainEvents", parser, evt.fetchMainEvents);
 module.exports = router;
