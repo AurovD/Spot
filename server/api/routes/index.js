7dc54cd2128bb81da0ea565fc64191eacf6e43d6
AurovD <dmitry8891@mail.ru>
2021-05-20 01:40:49 +0300

    test connection

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -3,10 +3,16 @@ const router = express.Router();
 const usr = require("../controllers/users");
 const evt = require("../controllers/events");
 const parser = require("body-parser").json();
+const cors = require('cors');
+
+let corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 router.post("/signup", parser, usr.signup);
 router.post("/login", parser, usr.login);
 router.get("/test", parser, usr.test);
-router.post("/createEvent", parser, evt.createEvent);
+router.post("/createEvent", cors(corsOptions), parser, evt.createEvent);
 router.get("/fetchMainEvents", parser, evt.fetchMainEvents);
 module.exports = router;
