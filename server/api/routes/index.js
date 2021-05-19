38cffb5c87c674b61daa6dcfffe18610be1e7e30
AurovD <dmitry8891@mail.ru>
2021-05-20 01:11:25 +0300

    test connection

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -3,23 +3,10 @@ const router = express.Router();
 const usr = require("../controllers/users");
 const evt = require("../controllers/events");
 const parser = require("body-parser").json();
-const cors = require("cors");
-
-let whitelist = ['https://spot.aurovd.ru', 'http://spot.aurovd.ru/', 'http://localhost:3000/']
-
-let corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
 
 router.post("/signup", parser, usr.signup);
 router.post("/login", parser, usr.login);
-router.get("/test", cors(corsOptions),parser, usr.test);
+router.get("/test", parser, usr.test);
 router.post("/createEvent", parser, evt.createEvent);
 router.get("/fetchMainEvents", parser, evt.fetchMainEvents);
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv = require('dotenv');
 const path = require('path');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8001;
 const app = express();
 const cors = require("cors");
 dotenv.config();
