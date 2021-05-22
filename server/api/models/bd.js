9249b12c2d2f8d97bf3e8254543aa6ad58073972
AurovD <dmitry8891@mail.ru>
2021-05-23 01:55:04 +0300

    api page

--- server/api/models/bd.js
+++ server/api/models/bd.js
@@ -1,20 +1,19 @@
-
 const Pool = require('pg').Pool
-// const pool = new Pool({
-//     user: 'babaduk',
-//     host: 'localhost',
-//     database: 'spot',
-//     password: '123',
-//     port: 5432,
-// });
-
 const pool = new Pool({
-    user: 'postgres',
+    user: 'babaduk',
     host: 'localhost',
     database: 'spot',
     password: '123',
     port: 5432,
 });
+
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'spot',
+//     password: '123',
+//     port: 5432,
+// });
 
 // pool.query("CREATE DATABASE spot;", (err, res) => {
 //     if(err) {
